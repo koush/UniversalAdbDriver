a0d25accb615e84f97ff33bd6bcc58a5704ac365
Koushik Dutta <koushd@gmail.com>
2015-02-22 19:34:58 -0800

    rename

--- catty.bat
+++ catty.bat
@@ -1,5 +1,4 @@
 inf2cat /driver:%CD%\usb_driver\ /os:XP_X86
-inf2cat /driver:%CD%\usb_driver\ /os:XP_X64
 
-signtool sign /v /s PrivateCertStore /n ClockworkMod /t http://timestamp.verisign.com/scripts/timstamp.dll usb_driver\androidwinusb86.cat
-signtool sign /v /s PrivateCertStore /n ClockworkMod /t http://timestamp.verisign.com/scripts/timstamp.dll usb_driver\androidwinusba64.cat
+
+inf2cat /driver:%CD%\usb_driver\ /os:XP_X64
