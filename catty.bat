bb30d5d5b9a2f29c2178358d14d1d82692b11581
Koushik Dutta <koushd@gmail.com>
2015-08-25 20:20:14 -0700

    10

--- catty.bat
+++ catty.bat
@@ -1,4 +1,4 @@
-inf2cat /driver:%CD%\usb_driver\ /os:XP_X86
+inf2cat /driver:%CD%\usb_driver\ /os:XP_X86,10_X86
 
 
-inf2cat /driver:%CD%\usb_driver\ /os:XP_X64
+inf2cat /driver:%CD%\usb_driver\ /os:XP_X64,10_X64
